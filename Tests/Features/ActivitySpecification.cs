d2e076edc2f3c5ceefe2063a973afb5b36f9995c
Colin Orr <colin.orr@gmail.com>
2016-02-11 18:57:37 +0000

    Fixed a typo in the activity spec

--- Tests/Features/ActivitySpecification.cs
+++ Tests/Features/ActivitySpecification.cs
@@ -54,7 +54,7 @@ namespace Tests.Features
                         specification.Logger.ToString().ShouldContain("An explosion");
                     };
 
-                    it["skips the following activites and assertions"] = () =>
+                    it["skips the following activities and assertions"] = () =>
                     {
                         executed.ShouldEqual(0);
                     };
