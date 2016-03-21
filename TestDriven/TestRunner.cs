591f8b1e6b70548ba53f29b3d87038b6b0cd9dde
Colin Orr <colin.orr@gmail.com>
2016-03-21 13:32:38 +0000

    Added an adapter for TestDriven.net

---
+++ TestDriven/TestRunner.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.AddInRunner;
+using System.Reflection;
+using TestDriven.Framework;
+
+namespace Penfold.TestDriven
+{
+    public class TestRunner : NUnitTestRunner, ITestRunner
+    {
+        public new TestRunState RunMember(ITestListener testListener, Assembly assembly, MemberInfo member)
+        {
+            if (member.MemberType == MemberTypes.Constructor)
+            {
+                return base.RunMember(testListener, assembly, member.DeclaringType);
+            }
+
+            return base.RunMember(testListener, assembly, member);
+        }
+    }
+}
